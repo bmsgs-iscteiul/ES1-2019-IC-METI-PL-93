f4bd58100d0b197ea1f790b8edb7ab41190e0f02
bmsgs-iscteiul <bmsgs@iscte-iul.pt>
2019-11-30 12:22:14 +0000

    First Commit

---
+++ 93_ES/src/main/java/_ES/_ES/App.java
@@ -0,0 +1,13 @@
+package _ES._ES;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
