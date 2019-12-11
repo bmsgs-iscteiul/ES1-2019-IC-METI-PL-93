5d5f2e74ff2a18243c484f9ec29947fdade9118e
cilgz-iscteiul <cilgz@iscte-iul.pt>
2019-12-11 12:52:32 +0000

    Documentaçao javaDoc PieChart - version 2.0

--- 93_ES/src/main/java/_ES/_ES/PieChart.java
+++ 93_ES/src/main/java/_ES/_ES/PieChart.java
@@ -16,7 +16,7 @@ import org.jfree.data.general.PieDataset;
 import org.jfree.util.Rotation;
 
 /**
- * A Classe PieChart tem como objetivo gerar gráficos circulares através da leitura da classe DataTable presente no projecto. 
+ * A Classe PieChart tem como objetivo gerar graficos circulares atraves da leitura da classe DataTable presente no projecto. 
  * @author Catarina Garcez - 82517
  * @version 2.0
  */
@@ -30,11 +30,11 @@ public class PieChart extends JPanel {
 	 * 
 	 * O construtor desta classe recebe tres argumentos de forma a conseguir construir o grafico circular. 
 	 * No construtor sao criados e inicializados diferentes tipos de objectos provinientes dos metodos criados nesta classe.
-	 * Antes da criçao do grafico definiou-se novos formatos para a geraçao de Labels de forma a ser mais simples a analise do grafico.
+	 * Antes da criacao do grafico definiou-se novos formatos para a geracao de Labels de forma a ser mais simples a analise do grafico.
 	 * Por fim e criado o grafico, indicando as dimensoes que o mesmo deve ocupar no painel e adicionado ao mesmo.
 	 * 
 	 * @param tableModel Tabela da qual se pretende retirar os dados.  
-	 * @param chartTitle Titulo que vai ser apresentado aquando a construção do grafico.
+	 * @param chartTitle Titulo que vai ser apresentado aquando a construçao do grafico.
 	 * @param column  Numero da coluna de onde vao ser lidos os dados.
 	 */
 
@@ -60,17 +60,17 @@ public class PieChart extends JPanel {
 	
 	/**
 	 * O metodo createDataset recebe o numero da coluna que se pretender ler e tem como finalidade devolver um dataset.
-	 * Primeiramente, e necessario contar o numero de linhas e os dados que cada linha apresenta. Esta informaçao e retirada da coluna escolhida da tableModel passada como argumento no construtor.
+	 * Primeiramente, e necessario contar o numero de linhas e os dados que cada linha apresenta. Esta informacao e retirada da coluna escolhida da tableModel passada como argumento no construtor.
 	 * Percorre-se todas as linhas atraves de um ForEach, em cada linha e criado objecto do valor presente na mesma e onde atraves de casts se transforma o objecto lido num Boolean. 
-	 * O contador previamente criado e inicializado a 0 é incrementado caso o Boolean lido na linha seja True.
-	 * Por fim são criados duas variaveis a serem apresentadas como resultados no grafico. Atraves do contador e do nemero de linhas percorridas e calculado o valor da variavel comErros. 
+	 * O contador previamente criado e inicializado a 0 e incrementado caso o Boolean lido na linha seja True.
+	 * Por fim sao criados duas variaveis a serem apresentadas como resultados no grafico. Atraves do contador e do numero de linhas percorridas e calculado o valor da variavel comErros. 
 	 * A segunda variavel(semErros) e facilmente calculada com base na primeira. Apes se definir os valores das variaveis estes sao atualizados no grafico.
 
 	 * @param column Numero da coluna de onde vao ser lidos os dados.
-	 * @return PieDataSet result, ou seja, os dados a serem apresentados no grafico provenientes da tableModel
+	 * @return PieDataSet result, ou seja, os dados a serem apresentados no grafico provenientes da tableModel.
 	 */
 
-	private PieDataset createDataset(int column) {
+	public PieDataset createDataset(int column) {
 
 		int rows = tableModel.getRowCount();
 		double counter = 0;
@@ -104,11 +104,11 @@ public class PieChart extends JPanel {
 	 * 
 	 * 
 	 * @param dataset PieDataSet, ou seja, os dados a serem apresentados no grafico provenientes da tableModel.
-	 * @param Title Titulo que vai ser apresentado aquando a construção do grafico.
+	 * @param Title Titulo que vai ser apresentado aquando a construcao do grafico.
 	 * @return JFreeChart, grafico circular 3D.
 	 */
 
-	private JFreeChart createChart(PieDataset dataset, String title) {
+	public JFreeChart createChart(PieDataset dataset, String title) {
 
 		JFreeChart chart = ChartFactory.createPieChart3D(
 				title,                  // chart title
