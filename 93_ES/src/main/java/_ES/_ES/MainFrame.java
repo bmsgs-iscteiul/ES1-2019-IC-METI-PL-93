315ff7917f6c8a0f207c77d41432a1039376e686
bmsgs-iscteiul <bmsgs@iscte-iul.pt>
2019-12-11 16:30:48 +0000

    JavaDoc da Classe MainFrame - atualização 7

--- 93_ES/src/main/java/_ES/_ES/MainFrame.java
+++ 93_ES/src/main/java/_ES/_ES/MainFrame.java
@@ -712,7 +712,9 @@ public class MainFrame {
 		frameDefine.setVisible(true);
 	}
 
-
+	/**
+	 * TODO
+	 */	
 	public static void main(String[] args) throws IOException {
 		@SuppressWarnings("unused")
 		MainFrame mainframe = new MainFrame();
